967d81310811328797b85b8078f8b9e81c5fbe12
Hanane2001 <hanan2123hanan@gmail.com>
2025-12-15 15:56:54 +0100

    create table users sans otp

--- sql/database.sql
+++ sql/database.sql
@@ -2,13 +2,24 @@
 CREATE DATABASE IF NOT EXISTS smart_wallet_Av;
 USE smart_wallet_Av;
 
+-- Create users table
+CREATE TABLE IF NOT EXISTS users(
+    idUser INT PRIMARY KEY AUTO_INCREMENT,
+    fullName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 -- Create incomes table
 CREATE TABLE IF NOT EXISTS incomes (
     idIn INT PRIMARY KEY AUTO_INCREMENT,
     amountIn DECIMAL(10,2) NOT NULL,
     dateIn DATE NOT NULL,
     descriptionIn VARCHAR(250) DEFAULT 'Unknown',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    idUser INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (idUser) REFERENCES users(idUser) ON DELETE CASCADE
 );
 
 -- Create expenses table
@@ -17,7 +28,9 @@ CREATE TABLE IF NOT EXISTS expenses (
     amountEx DECIMAL(10,2) NOT NULL,
     dateEx DATE NOT NULL,
     descriptionEx VARCHAR(250) DEFAULT 'Unknown',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    idUser INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (idUser) REFERENCES users(idUser) ON DELETE CASCADE
 );
 
 INSERT INTO incomes (amountIn, dateIn, descriptionIn) VALUES 
